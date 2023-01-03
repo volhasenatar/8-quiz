88e5beeb65578c2b3b8d6e36d11b8042ce2bd4cd
VOLGA SENATAR <106486711+volhasenatar@users.noreply.github.com>
2023-01-04 00:04:42 +0300

    Add files via upload

---
+++ src/components/Game.js
@@ -0,0 +1,25 @@
+import { questions } from './questions';
+
+export function Game({ question, onClickVariant, step }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  return (
+    <>
+      <h1>Test "Angielskie czasowniki nieregularne"</h1>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h3>{question.title}</h3>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li onClick={() => onClickVariant(index)} key={text}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
